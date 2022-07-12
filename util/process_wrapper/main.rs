340f791b60551fbf8d3076c43004054b2d7f5f7c
Parker Timmerman <parker@parkertimmerman.com>
2022-07-12 18:02:27 -0400

    debug debug debug

--- util/process_wrapper/main.rs
+++ util/process_wrapper/main.rs
@@ -55,8 +55,10 @@ fn main() {
         Ok(v) => v,
     };
 
-    let pwd = std::env::current_dir();
-    panic!("current dir: {:?}", pwd);
+    let pwd = std::env::current_dir().expect("must get path");
+    let list = std::fs::read_dir(&pwd).expect("read dir");
+    let entries: Vec<_> = list.collect();
+    panic!("current dir: {:?}, entries: {:?}", pwd, entries);
 
     let stderr: Box<dyn io::Write + Send> = if let Some(stderr_file) = opts.stderr_file {
         Box::new(
