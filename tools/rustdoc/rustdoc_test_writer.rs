9e3f0a029a49134bd1068c12c64157e3d164cac2
Parker Timmerman <parker@parkertimmerman.com>
2022-07-12 17:38:16 -0400

    another one

--- tools/rustdoc/rustdoc_test_writer.rs
+++ tools/rustdoc/rustdoc_test_writer.rs
@@ -134,7 +134,7 @@ fn write_test_runner_unix(
 ) {
     let mut content = vec![
         "#!/usr/bin/env bash".to_owned(),
-        "pwd > ~/pwd.txt".to_owned(),
+        "pwd".to_owned(),
         "".to_owned(),
         "exec env - \\".to_owned(),
     ];
